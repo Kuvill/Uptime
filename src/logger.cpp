b91e1e997630dd3be4df55abdfd0f762a51da04d
-kuvill <lenyavr@gmail.com>
2025-08-19 17:23:40 +0200

    again minor logger

--- src/logger.cpp
+++ src/logger.cpp
@@ -2,17 +2,26 @@
 #include "common/change_dir.hpp"
 #include "common/aliases.hpp"
 
+// thx for +300ms to compilation just for untie
+#include <iostream>
+
 #include <fstream>
 
 #include <sys/stat.h>
 #include <unistd.h>
 
-Logger::Logger( LogLvl lvl ) : _lvl(lvl) {}
-Logger::Logger( std::ofstream* of, LogLvl lvl ) : _lvl(lvl) {
+Logger::Logger( LogLvl lvl ) : _lvl(lvl) {
+    // since i use only logger to interact with user
+    std::ios_base::sync_with_stdio( false );
+    std::clog.tie( nullptr );
+}
+
+
+Logger::Logger( std::ofstream* of, LogLvl lvl ) : Logger(lvl) {
     logger.log(LogLvl::Info, "Logger source has been changed.");
     _out = of;
 }
-Logger::Logger( const char* path, LogLvl lvl ) : _lvl(lvl) {
+Logger::Logger( const char* path, LogLvl lvl ) : Logger(lvl) {
     CheckDirectory();
     logger.log(LogLvl::Info, "Logger source has been changed.");
     _out = new std::ofstream( path );
