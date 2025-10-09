2631040cc5a8ca9ebdaa9a2e38b857ba95f44e5f
kuvill <lenyavr@gmail.com>
2025-10-09 19:57:59 +0200

    one of the steps

--- src/demon/plugin.cpp
+++ src/demon/plugin.cpp
@@ -1,12 +1,25 @@
+#include "common/time.hpp"
+#include <chrono>
 #include <demon/ram_storage.hpp>
 #include <demon/db.hpp>
 
 extern Storage* g_store;
 extern Database* g_db;
 
+static ProcessInfo prevName;
+static bool prevSkipped = false;
+
 void saveProcessInfo( const ProcessInfo& info ) {
-    if( !info.name.empty() )
-        g_store->insert( info );
+    if( info.name.empty() )
+        return;
+
+    if( getCurrentTime() - prevName.uptime <= std::chrono::seconds(1) );
+
+    if( prevSkipped ) {
+        
+    }
+
+    g_store->insert( info );
 }
 
 
